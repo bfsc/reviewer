6ae63de4f04df6f026af76fe9c6f8393165882bc
bfsc <brunocartaxo@gmail.com>
2013-01-27 00:30:47 -0200

    Adding IEEE search provider project.

---
+++ code/br.cin.ufpe.reviewer.search.provider.ieee/src/br/cin/ufpe/reviewer/search/provider/ieee/IeeeSearchProvider.java
@@ -0,0 +1,16 @@
+package br.cin.ufpe.reviewer.search.provider.ieee;
+
+import java.util.List;
+
+import br.cin.ufpe.reviewer.search.provider.spi.SearchProvider;
+import br.cin.ufpe.reviewer.search.provider.spi.entities.Study;
+import br.cin.ufpe.reviewer.search.provider.spi.expetions.SearchProviderException;
+
+public class IeeeSearchProvider implements SearchProvider {
+
+	public List<Study> search(String searchString) throws SearchProviderException {
+		//TODO IMPLEMENTAR
+		return null;
+	}
+
+}
