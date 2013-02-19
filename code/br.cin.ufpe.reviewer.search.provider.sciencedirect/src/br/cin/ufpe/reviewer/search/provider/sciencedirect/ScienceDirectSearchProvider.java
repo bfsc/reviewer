8edd01eab83d5391e4169a36f7e73e5c396e953a
Pedro <Pedro@Casa-PC.home>
2013-02-18 22:20:05 -0300

    Testando commit

--- code/br.cin.ufpe.reviewer.search.provider.sciencedirect/src/br/cin/ufpe/reviewer/search/provider/sciencedirect/ScienceDirectSearchProvider.java
+++ code/br.cin.ufpe.reviewer.search.provider.sciencedirect/src/br/cin/ufpe/reviewer/search/provider/sciencedirect/ScienceDirectSearchProvider.java
@@ -7,7 +7,19 @@ import br.cin.ufpe.reviewer.search.provider.spi.entities.Study;
 import br.cin.ufpe.reviewer.search.provider.spi.expetions.SearchProviderException;
 
 public class ScienceDirectSearchProvider implements SearchProvider {
+	
+	private static final String DOMAIN_DL_SCIENCE_DIRECT = "http://www.sciencedirect.com/";
+	
+	private static final String XPATH_ADVANCED_SEARCH = "//td[@align='right' and @nowrap='nowrap' and @widht='90%' and @valign='middle']//a[@style='vertical-align:bottom;font-size:0.92em;']";
+	
+	private static final String XPATH_EXPERT_SEARCH = "//div[@class='advExpertLink' and @style='float:right;']";
+	
+	
 
+	
+	
+	
+	
 	public List<Study> search(String searchString) throws SearchProviderException {
 		return null;
 	}
