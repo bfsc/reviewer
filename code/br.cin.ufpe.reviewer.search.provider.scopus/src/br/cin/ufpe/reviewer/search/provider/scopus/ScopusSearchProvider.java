518dbf9d799ddf7b9db6ff66d0ac9fbfaea89333
bfsc <brunocartaxo@gmail.com>
2013-02-28 22:12:58 -0300

    Initial implementation of SCOPUS search provider (incomplete).

--- code/br.cin.ufpe.reviewer.search.provider.scopus/src/br/cin/ufpe/reviewer/search/provider/scopus/ScopusSearchProvider.java
+++ code/br.cin.ufpe.reviewer.search.provider.scopus/src/br/cin/ufpe/reviewer/search/provider/scopus/ScopusSearchProvider.java
@@ -1,8 +1,5 @@
 package br.cin.ufpe.reviewer.search.provider.scopus;
 
-import java.io.FileWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -11,26 +8,16 @@ import br.cin.ufpe.reviewer.search.provider.spi.entities.Study;
 import br.cin.ufpe.reviewer.search.provider.spi.expetions.SearchProviderException;
 
 import com.gargoylesoftware.htmlunit.Page;
-import com.gargoylesoftware.htmlunit.ScriptResult;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlCheckBoxInput;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlDivision;
 import com.gargoylesoftware.htmlunit.html.HtmlOption;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlScript;
 import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
-import com.gargoylesoftware.htmlunit.util.WebClientUtils;
 
 public class ScopusSearchProvider implements SearchProvider {
-
-	private static final String URL_SCOPUS_1 = "http://www.scopus.com/results/results.url?sort=plf-f&src=s&sid=";
-	private static final String URL_SCOPUS_2 = "&sot=a&sdt=a&sl=30&s=";
-	private static final String URL_SCOPUS_3 = "&origin=searchadvanced&txGid=";
-
-	private static final String URL_ENCODE_UTF_8 = "UTF-8";
-	private static final String URL_ENCODE_ISO_8859_1 = "ISO-8859-1";
 
 	public List<Study> search(String searchString) throws SearchProviderException {
 		List<Study> toReturn = new LinkedList<Study>();
@@ -47,16 +34,14 @@ public class ScopusSearchProvider implements SearchProvider {
 
 			// Obtaining the TXGID in order to compose the search URL 
 			HtmlPage advancedSearchPage = browser.getPage("http://www.scopus.com/search/form.url?display=advanced");
-			HtmlInput txGidInput = (HtmlInput) advancedSearchPage.getFirstByXPath("//input[@type='hidden' and @id='txGid']");
-			String txGid = txGidInput.getValueAttribute();
 			
-			HtmlInput input = (HtmlInput) advancedSearchPage.getFirstByXPath("//input[@type='hidden' and @id='txGid']");
-			
-			// Assemble search URL
-			String searchUrl = assembleSearchUrl(searchString, txGid);
+			HtmlDivision searchDiv = advancedSearchPage.getFirstByXPath("//div[@id='searchfield']");
+			searchDiv.setTextContent(searchString);
+			HtmlSubmitInput searchButton = advancedSearchPage.getFirstByXPath("//input[@type='submit' and @value='Search']");
+			HtmlPage resultsPage = searchButton.click();
 			
 			// Extract studies data
-			toReturn.addAll(extractStudiesData(browser, searchUrl));
+			toReturn.addAll(extractStudiesData(resultsPage));
 		} catch (Exception e) {
 			throw new SearchProviderException("An error occurred trying to search the following query string:" + searchString, e);
 		}
@@ -64,38 +49,11 @@ public class ScopusSearchProvider implements SearchProvider {
 		return  toReturn;
 	}
 	
-	private String assembleSearchUrl(String searchString, String txGid) {
-		String encodedSearchString = "";
-		String encodedTxGid= "";
-		
-		try {
-			encodedSearchString = URLEncoder.encode(searchString, URL_ENCODE_ISO_8859_1).toString();
-		} catch (UnsupportedEncodingException e) {
-			try {
-				encodedSearchString = URLEncoder.encode(searchString, URL_ENCODE_UTF_8).toString();
-			} catch (UnsupportedEncodingException e2) {
-				throw new RuntimeException("An error occurred trying to encode the url.", e2);
-			}
-		}
-		
-		try {
-			encodedTxGid = URLEncoder.encode(txGid, URL_ENCODE_ISO_8859_1).toString();
-		} catch (UnsupportedEncodingException e) {
-			try {
-				encodedTxGid = URLEncoder.encode(txGid, URL_ENCODE_UTF_8).toString();
-			} catch (UnsupportedEncodingException e2) {
-				throw new RuntimeException("An error occurred trying to encode the url.", e2);
-			}
-		}
-		
-		return URL_SCOPUS_1 + encodedTxGid + URL_SCOPUS_2 + encodedSearchString + URL_SCOPUS_3 + encodedTxGid;
-	}
-	
-	private List<Study> extractStudiesData(WebClient browser, String searchUrl) {
+	private List<Study> extractStudiesData(HtmlPage page) {
 		List<Study> toReturn = new LinkedList<Study>();
 		
 		try {
-			HtmlPage page = browser.getPage(searchUrl);
+//			HtmlPage page = browser.getPage(searchUrl);
 			
 			HtmlCheckBoxInput checkboxInput = page.getFirstByXPath("//input[@type='checkbox' and @name='selectAllCheckBox']");
 			checkboxInput.click();
@@ -125,10 +83,9 @@ public class ScopusSearchProvider implements SearchProvider {
 			HtmlSubmitInput exportButton = exportPage.getFirstByXPath("//input[@type='submit' and @value='Export' and @class='jsEnabled Bold']");
 			Page exportedStudies = exportButton.click();
 			
-			FileWriter writer = new FileWriter("C:/Documents and Settings/Bruno Cartaxo/Desktop/page.html");
-			writer.write(exportedStudies.getWebResponse().getContentAsString());
-			writer.flush();
-			writer.close();
+			System.out.println(exportedStudies.getWebResponse().getContentAsString());
+			
+			// TODO FAZER O PARSER DO BIBTEX
 		} catch (Exception e) {
 			//TRATAR EXCECAO
 			e.printStackTrace();
