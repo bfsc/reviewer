2db4eae7465df44f84456a2e00630cf6a9755c37
vsnepomuceno <vnepomuceno@gmail.com>
2013-05-10 16:14:48 -0300

    Logger Added

---
+++ code/br.ufpe.cin.reviewer.logger/src/br/ufpe/cin/reviewer/logger/ReviewerLogger.java
@@ -0,0 +1,42 @@
+package br.ufpe.cin.reviewer.logger;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.contexts.ContextInjectionFactory;
+import org.eclipse.e4.core.contexts.EclipseContextFactory;
+import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.services.log.Logger;
+import org.eclipse.e4.ui.internal.workbench.WorkbenchLogger;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+@SuppressWarnings("restriction")
+public class ReviewerLogger {
+
+	@Inject 	
+	private static Logger log;
+	
+	static {
+		Bundle bundle = FrameworkUtil.getBundle(ReviewerLogger.class);
+		BundleContext bundleContext = bundle.getBundleContext();
+		IEclipseContext eclipseCtx =   
+				EclipseContextFactory.getServiceContext(bundleContext);
+
+		// Create instance of class
+		log = ContextInjectionFactory.make(WorkbenchLogger.class, eclipseCtx);
+	}
+	
+	public static void error(String message) {
+		log.error(message);
+	}
+	
+	public static void warn(String message) {
+		log.warn(message);
+	}
+	
+	public static void debug(String message) {
+		log.debug(message);
+	}
+	
+}
