f41253c90b116e075010a185b7a01103207232a3
vsnepomuceno <vnepomuceno@gmail.com>
2013-05-30 19:42:03 -0300

    issue #50

---
+++ code/br.ufpe.cin.reviewer.searchprovider.spi/src/br/ufpe/cin/reviewer/searchprovider/spi/exceptions/SearchProviderError.java
@@ -0,0 +1,12 @@
+package br.ufpe.cin.reviewer.searchprovider.spi.exceptions;
+
+public enum SearchProviderError {
+
+	// common
+	SEARCH_PROVIDER_COMMON_ERROR,
+	// acm
+	SEARCH_PROVIDER_ACM_ERROR_FORBIDDEN,
+	// scopus and science direct
+	SEARCH_PROVIDER_SCOPUS_ERROR_LOGIN_REQUIRED	
+	
+}
