3b65b4c4823e814302ea43a020caa0398cb59cc9
caiocaladoo <caio.caladoo@gmail.com>
2013-09-21 10:42:12 -0300

    Add new model classes

--- code/br.ufpe.cin.reviewer.searchprovider.acm/src/br/ufpe/cin/reviewer/searchprovider/acm/AcmSearchProvider.java
+++ code/br.ufpe.cin.reviewer.searchprovider.acm/src/br/ufpe/cin/reviewer/searchprovider/acm/AcmSearchProvider.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import br.ufpe.cin.reviewer.model.common.Study;
+import br.ufpe.cin.reviewer.model.study.Study;
 import br.ufpe.cin.reviewer.searchprovider.spi.SearchProvider;
 import br.ufpe.cin.reviewer.searchprovider.spi.SearchProviderResult;
 import br.ufpe.cin.reviewer.searchprovider.spi.exceptions.SearchProviderError;
--- code/br.ufpe.cin.reviewer.searchprovider.ieee/src/br/ufpe/cin/reviewer/searchprovider/ieee/IeeeSearchProvider.java
+++ code/br.ufpe.cin.reviewer.searchprovider.ieee/src/br/ufpe/cin/reviewer/searchprovider/ieee/IeeeSearchProvider.java
@@ -4,7 +4,7 @@ import java.net.URLEncoder;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-import br.ufpe.cin.reviewer.model.common.Study;
+import br.ufpe.cin.reviewer.model.study.Study;
 import br.ufpe.cin.reviewer.searchprovider.spi.SearchProvider;
 import br.ufpe.cin.reviewer.searchprovider.spi.SearchProviderResult;
 import br.ufpe.cin.reviewer.searchprovider.spi.exceptions.SearchProviderError;
