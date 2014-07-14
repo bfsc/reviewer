78035657b04630936c8c6e6cea657dce9a2217c4
arthurmoreno <arthurmoreno_6@hotmail.com>
2014-07-14 10:09:54 -0300

    Closing issue #70

--- code/br.ufpe.cin.reviewer.ui.rcp/src/br/ufpe/cin/reviewer/ui/rcp/literaturereview/StudyEvaluatorModeView.java
+++ code/br.ufpe.cin.reviewer.ui.rcp/src/br/ufpe/cin/reviewer/ui/rcp/literaturereview/StudyEvaluatorModeView.java
@@ -3,8 +3,12 @@ package br.ufpe.cin.reviewer.ui.rcp.literaturereview;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.forms.widgets.Section;
 
@@ -20,6 +24,8 @@ public class StudyEvaluatorModeView extends BaseView {
 	private Composite sectionComposite;
 
 	private Text titleText;
+	private Table studiesTable;
+	private Button saveButton;
 	
 	public StudyEvaluatorModeView() {
 		ReviewerViewRegister.putView(ID, this);
@@ -49,6 +55,35 @@ public class StudyEvaluatorModeView extends BaseView {
 
 		Label titleLabel = toolkit.createLabel(sectionComposite, "Title: ");
 		Label titleNameLabel = toolkit.createLabel(sectionComposite, "Titulo");
+
+		//Info Table
+		studiesTable = toolkit.createTable(sectionComposite, SWT.BORDER | SWT.FULL_SELECTION);
+		studiesTable.setLinesVisible (true);
+		studiesTable.setHeaderVisible (true);
+		GridData studiesTableLayoutData = new GridData(GridData.FILL_BOTH);
+		studiesTableLayoutData.horizontalSpan = 2;
+		studiesTable.setLayoutData(studiesTableLayoutData);
+		//studiesTable.addMouseListener(new StudyMouseHandler());
+		
+		//insert columns and set their names
+		String[] titlesStudies = {"CODE v", "STATUS v", "GROUP v", "TITLE v", "YEAR v"};
+		for (int i=0; i<titlesStudies.length; i++) {
+			TableColumn column = new TableColumn (studiesTable, SWT.CENTER);
+			column.setText (titlesStudies [i]);
+		}
+		for (int i=0; i<titlesStudies.length; i++) {
+			studiesTable.getColumn (i).pack ();
+		}
+		
+		TableItem item = new TableItem(studiesTable,SWT.NONE);
+		item.setText("item1");
+		item.setText(0, "code1");
+
+		saveButton = toolkit.createButton(sectionComposite, "Save", SWT.PUSH);
+		GridData importButtonLayoutData = new GridData();
+		importButtonLayoutData.horizontalAlignment = SWT.RIGHT;
+		importButtonLayoutData.horizontalSpan = 2;
+		saveButton.setLayoutData(importButtonLayoutData);
 
 		section.setClient(sectionComposite);
 	}
